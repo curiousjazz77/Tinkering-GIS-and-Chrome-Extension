26c73db9244689451401f7b5d113a795049f2520
Jazz <10506008+curiousjazz77@users.noreply.github.com>
2018-09-15 02:29:28 -0500

    Update network_topography.sql

--- PostGISdataload/Help_Santa_Distribution/network_topography.sql
+++ PostGISdataload/Help_Santa_Distribution/network_topography.sql
@@ -7,3 +7,15 @@
 *);
 */
 
+//Start with adding two additional columns to our roads table.
+
+ALTER TABLE roads ADD COLUMN source integer;
+ALTER TABLE roads ADD COLUMN target integer;
+CREATE INDEX roads_source_idx ON roads (source);
+CREATE INDEX roads_target_idx ON roads (target);
+
+//And then use pgr_createTopology function which builds a network topology based on the geometry information (it analyses roads geometry and automatically assigns node ids to the source and target columns).
+
+SELECT pgr_createTopology('roads', 0.0001, 'geom', 'id'); 
+
+//After about 5 minutes of calculations the dataset will be ready.
