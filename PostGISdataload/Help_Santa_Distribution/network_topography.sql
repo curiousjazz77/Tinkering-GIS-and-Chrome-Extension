4ec7ab404c9132f9d4dffab77cbfce3ce79b5a1b
Jazz <10506008+curiousjazz77@users.noreply.github.com>
2018-09-15 02:30:10 -0500

    Update network_topography.sql

--- PostGISdataload/Help_Santa_Distribution/network_topography.sql
+++ PostGISdataload/Help_Santa_Distribution/network_topography.sql
@@ -1,4 +1,4 @@
-/*download: https://www.dropbox.com/s/bu9k0k9yfb4ktw3/roads.sql.gz
+/* download: https://www.dropbox.com/s/bu9k0k9yfb4ktw3/roads.sql.gz
 * gunzip -c roads.sql.gz | psql pgr_tutorial
 * The above command creates one table from the dataset:
 * CREATE TABLE roads (
@@ -14,8 +14,11 @@ ALTER TABLE roads ADD COLUMN target integer;
 CREATE INDEX roads_source_idx ON roads (source);
 CREATE INDEX roads_target_idx ON roads (target);
 
-//And then use pgr_createTopology function which builds a network topology based on the geometry information (it analyses roads geometry and automatically assigns node ids to the source and target columns).
+/* And then use pgr_createTopology function which builds a network topology 
+* based on the geometry information (it analyses roads geometry and automatically 
+* assigns node ids to the source and target columns).
+*/
 
 SELECT pgr_createTopology('roads', 0.0001, 'geom', 'id'); 
 
-//After about 5 minutes of calculations the dataset will be ready.
+/* After about 5 minutes of calculations the dataset will be ready. */
