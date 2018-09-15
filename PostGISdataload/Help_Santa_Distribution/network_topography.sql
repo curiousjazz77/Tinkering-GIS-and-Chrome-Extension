7422262bc6fb381fa7c7b0b35aa01880396e7393
Jazz <10506008+curiousjazz77@users.noreply.github.com>
2018-09-15 02:31:14 -0500

    Update network_topography.sql

--- PostGISdataload/Help_Santa_Distribution/network_topography.sql
+++ PostGISdataload/Help_Santa_Distribution/network_topography.sql
@@ -1,4 +1,5 @@
-/* download: https://www.dropbox.com/s/bu9k0k9yfb4ktw3/roads.sql.gz
+/* SOURCE: https://blog.daftcode.pl/find-your-way-with-the-power-of-postgis-pgrouting-66d620ef201b
+* download: https://www.dropbox.com/s/bu9k0k9yfb4ktw3/roads.sql.gz
 * gunzip -c roads.sql.gz | psql pgr_tutorial
 * The above command creates one table from the dataset:
 * CREATE TABLE roads (
@@ -22,3 +23,17 @@ CREATE INDEX roads_target_idx ON roads (target);
 SELECT pgr_createTopology('roads', 0.0001, 'geom', 'id'); 
 
 /* After about 5 minutes of calculations the dataset will be ready. */
+
+/* Start navigating! 🚗💨
+* After this short introduction let’s move to some real-world 
+* examples and find the shortest path between nodes #1 
+* and #5000 using Dijkstra algorithm.
+*/
+
+SELECT seq, node, edge, cost as cost, agg_cost, geom
+FROM pgr_dijkstra(
+   'SELECT id, source, target, st_length(geom, true) as cost FROM roads',
+   1,
+   5000
+) as pt
+JOIN roads rd ON pt.edge = rd.id;
